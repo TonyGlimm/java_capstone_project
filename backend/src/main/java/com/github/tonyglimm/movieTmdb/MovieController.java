5e9fc4574908ea925dc9ccdb83647dedddaeca64
Tony Glimm <glimm.tony@gmail.com>
2023-03-22 16:34:20 +0100

    fixed get route

--- backend/src/main/java/com/github/tonyglimm/movieTmdb/MovieController.java
+++ backend/src/main/java/com/github/tonyglimm/movieTmdb/MovieController.java
@@ -2,11 +2,13 @@ package com.github.tonyglimm.movieTmdb;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MovieController {
     private final MovieService movieService;
 
@@ -15,7 +17,7 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping("/movies/popular")
     public List<Movie> getPopularMovies() {
         return movieService.getPopularMovies();
     }
