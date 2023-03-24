d9f36a2ea2ebe430c8693bbf009dc241ca0f4e70
Tony Glimm <glimm.tony@gmail.com>
2023-03-24 11:08:38 +0100

    added images to the MovieCard.tsx and slightly reformatted the MovieCard.css accordingly

--- frontend/src/components/MovieCard/MovieCard.tsx
+++ frontend/src/components/MovieCard/MovieCard.tsx
@@ -5,15 +5,27 @@ import {Movie} from "../../models/Movie";
 
 type MovieCardProps = {
     movie: Movie
+
+
 }
+
+
 
 function MovieCard(props: MovieCardProps) {
-    return (
-        <div className="gallery__card" id={props.movie.id.toString()}>
-            <h2 className="gallery__card--title">{props.movie.original_title}</h2>
-            <h3 className="gallery__card--date">{props.movie.release_date}</h3>
-        </div>
-    )
-}
 
+    let poster_path = `https://image.tmdb.org/t/p/w342${props.movie.poster_path}`
+    if (props.movie.poster_path == null) {
+        poster_path = "https://i.imgur.com/wjVuAGb.png"
+    }
+        return (
+            <div className="gallery__card" id={props.movie.id.toString()}>
+                <h2 className="gallery__card--title">{props.movie.original_title}</h2>
+
+                <img className="glimm-image" title={props.movie.original_title} src={poster_path}
+                     alt={props.movie.original_title}/>
+
+            </div>
+        )
+
+}
 export default MovieCard;
