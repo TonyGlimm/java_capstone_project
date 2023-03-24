889a178a1e97dfc4d9ce47abe975396282471688
Tony Glimm <glimm.tony@gmail.com>
2023-03-24 16:42:05 +0100

    installed MUI and updated the styling of the frontend

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+import Header from "../Header/Header";
 import MovieGallery from "../MovieGallery/MovieGallery";
 import useMoviesApi from "../hooks/useMoviesApi";
 
@@ -9,11 +10,16 @@ function App() {
   const {loading, movies} = useMoviesApi();
 
   return (
-    <div className="App">
-    <main className={"main"}>
+      <>
+        <Header/>
+        <div className="app">
+      <main className={"main"}>
+
       {!loading && <MovieGallery movies={movies}/>}
     </main>
+
     </div>
+      </>
   );
 }
 
