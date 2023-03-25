e570f93fc0d53cbd3fb440d20f13bb51524ebdd2
Tony Glimm <glimm.tony@gmail.com>
2023-03-25 19:16:17 +0100

    MUI styling. installed Bottom Navigation Bar

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -4,22 +4,31 @@ import './App.css';
 import Header from "../Header/Header";
 import MovieGallery from "../MovieGallery/MovieGallery";
 import useMoviesApi from "../hooks/useMoviesApi";
+import SimpleBottomNavigation from "../MainNav";
+import {Container} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
   const {loading, movies} = useMoviesApi();
 
   return (
-      <>
+      <BrowserRouter>
         <Header/>
         <div className="app">
       <main className={"main"}>
-
+{/*
       {!loading && <MovieGallery movies={movies}/>}
+*/}
     </main>
+                <Routes>
+                    <Route path={"/"} element={<MovieGallery movies={movies}/>}/>
 
+                </Routes>
+            <SimpleBottomNavigation/>
     </div>
-      </>
+
+      </BrowserRouter>
   );
 }
 
