48226b57ab19984c4e36d8ac97e666b5c5c231f6
Tony Glimm <glimm.tony@gmail.com>
2023-03-29 11:12:30 +0200

    adjustments to the badge for voteAverage

--- frontend/src/components/TrendingCard/TrendingCard.tsx
+++ frontend/src/components/TrendingCard/TrendingCard.tsx
@@ -16,7 +16,7 @@ function MovieCard(props: TrendingCardProps) {
     }
     return (
         <div className="media">
-            <Badge badgeContent={props.movie.voteAverage} color="primary"/>
+            <Badge badgeContent={props.movie.voteAverage} color={props.movie.voteAverage >7? "primary":"secondary"}/>
             <img className="poster" title={props.movie.originalTitle} src={poster_path}
                  alt={props.movie.originalTitle}/>
             <h2 className="title">{props.movie.originalTitle}</h2>
