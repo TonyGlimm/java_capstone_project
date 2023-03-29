4cca65d6acfdce009c5db096328dd90cf7dffc11
Tony Glimm <glimm.tony@gmail.com>
2023-03-29 13:38:34 +0200

    centered pagination and made it more easy to look at

--- frontend/src/components/Pagination/CustomPagination.tsx
+++ frontend/src/components/Pagination/CustomPagination.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
+import {createTheme, ThemeProvider} from "@mui/material";
 
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 type CustomPaginationProps = {
     setPage: React.Dispatch<React.SetStateAction<number>>,
@@ -14,10 +21,21 @@ function CustomPagination(props: CustomPaginationProps) {
     }
 
         return (
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: 10,
+                }}
+            >
+            <ThemeProvider theme={darkTheme}>
             <Pagination count={props.numberOfPages}
                         onChange={(event, value) => handlePageChange(value)}
                         color="primary" hideNextButton
                         hidePrevButton/>
+            </ThemeProvider>
+            </div>
         );
 
 }
